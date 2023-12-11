e97edc1f25cd03bf23127413a03f6c7f3180fd9e
Malik <malikhussein229@gmail.com>
2023-12-11 08:58:08 -0800

    Creating files

---
+++ str_func3.c
@@ -0,0 +1,56 @@
+#include "shell.h"
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, j;
+	char *s = dest;
+
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	if (i < n)
+	{
+		j = i;
+
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+	char *s = dest;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	if (j < n)
+		dest[i] = '\0';
+
+	return (s);
+}
+
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
