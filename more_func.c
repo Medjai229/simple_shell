b88341222ef31b8b481f5e01702fbdfd79797b7e
Malik <malikhussein229@gmail.com>
2023-12-13 09:06:48 -0800

    Fixing betty errors

--- more_func.c
+++ more_func.c
@@ -1,11 +1,23 @@
 #include "shell.h"
 
+/**
+ * interactive - returns true if shell is interactive mode
+ * @info: struct address
+ *
+ * Return: 1 if interactive mode, 0 otherwise
+ */
 int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
-int is_delim(char c, char *delim)
+/**
+ * _isdelim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
+ */
+int _isdelim(char c, char *delim)
 {
 	while (*delim)
 	{
@@ -15,6 +27,11 @@ int is_delim(char c, char *delim)
 	return (0);
 }
 
+/**
+ *_isalpha - checks for alphabetic character
+ *@c: The character to input
+ *Return: 1 if c is alphabetic, 0 otherwise
+ */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
@@ -23,6 +40,11 @@ int _isalpha(int c)
 		return (0);
 }
 
+/**
+ *_atoi - converts a string to an integer
+ *@s: the string to be converted
+ *Return: 0 if no numbers in string, converted number otherwise
+ */
 int _atoi(char *s)
 {
 	int i, sign = 1, flag = 0, output;
