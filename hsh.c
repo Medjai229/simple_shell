25b994a3cf67c925be5a827e0935533fe11d0a2b
MMarzoo <mmarzouk12378@gmail.com>
2023-12-13 05:41:12 -0800

    creating files

--- hsh.c
+++ hsh.c
@@ -38,40 +38,29 @@ int hsh(info_t *info, char **av)
 }
 
 /***/
-void find_cmd(info_t *info)
+int find_builtin(info_t *info)
 {
-	char *path = NULL;
-	int i, k;
+	int i, built_in_ret = -1;
+	builtin_table builtintbl[] = {
+		{"exit", _myexit},
+		{"env", _myenv},
+		{"help", _myhelp},
+		{"history", _myhistory},
+		{"setenv", _mysetenv},
+		{"unsetenv", _myunsetenv},
+		{"cd", _mycd},
+		{"alias", _myalias},
+		{NULL, NULL}
+	};
 
-	info->path = info->argv[0];
-	if (info->linecount_flag == 1)
-	{
-		info->line_count++;
-		info->linecount_flag = 0;
-	}
-	for (i = 0, k = 0; info->arg[i]; i++)
-		if (!is_delim(info->arg[i], " \t\n"))
-			k++;
-	if (!k)
-		return;
-
-	path = find_path(info, _getenv(info, "PATH="), info->argv[0]);
-	if (path)
-	{
-		info->path = path;
-		fork_cmd(info);
-	}
-	else
-	{
-		if ((interactive(info) || _getenv(info, "PATH=")
-			|| info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
-			fork_cmd(info);
-		else if (*(info->arg) != '\n')
+	for (i = 0; builtintbl[i].type; i++)
+		if (_strcmp(info->argv[0], builtintbl[i].type) == 0)
 		{
-			info->status = 127;
-			print_error(info, "not found\n");
+			info->line_count++;
+			built_in_ret = builtintbl[i].func(info);
+			break;
 		}
-	}
+	return (built_in_ret);
 }
 
 /***/
