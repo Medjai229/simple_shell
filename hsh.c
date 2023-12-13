8dbdfa8c68331dbe9ae5d5dfb1dfbf365d114e29
MMarzoo <mmarzouk12378@gmail.com>
2023-12-13 05:43:26 -0800

    fixed and add betty

--- hsh.c
+++ hsh.c
@@ -1,6 +1,12 @@
 #include "shell.h"
 
-/***/
+/**
+ * hsh - main shell loop
+ * @info: the parameter & return info struct
+ * @av: the argument vector from main()
+ *
+ * Return: 0 on success, 1 on error, or error code
+ */
 int hsh(info_t *info, char **av)
 {
 	ssize_t r = 0;
@@ -37,7 +43,15 @@ int hsh(info_t *info, char **av)
 	return (builtin_ret);
 }
 
-/***/
+/**
+ * find_builtin - finds a builtin command
+ * @info: the parameter & return info struct
+ *
+ * Return: -1 if builtin not found,
+ *			0 if builtin executed successfully,
+ *			1 if builtin found but not successful,
+ *			-2 if builtin signals exit()
+ */
 int find_builtin(info_t *info)
 {
 	int i, built_in_ret = -1;
@@ -63,7 +77,12 @@ int find_builtin(info_t *info)
 	return (built_in_ret);
 }
 
-/***/
+/**
+ * find_cmd - finds a command in PATH
+ * @info: the parameter & return info struct
+ *
+ * Return: void
+ */
 void find_cmd(info_t *info)
 {
 	char *path = NULL;
@@ -100,7 +119,12 @@ void find_cmd(info_t *info)
 	}
 }
 
-/***/
+/**
+ * fork_cmd - forks a an exec thread to run cmd
+ * @info: the parameter & return info struct
+ *
+ * Return: void
+ */
 void fork_cmd(info_t *info)
 {
 	pid_t child_pid;
