bf3e52a91fc8be335f712480521aec03b660c7cb
Malik <malikhussein229@gmail.com>
2023-12-13 09:06:29 -0800

    Fixing betty errors

--- mem_func.c
+++ mem_func.c
@@ -1,5 +1,12 @@
 #include "shell.h"
 
+/**
+ **_memset - fills memory with a constant byte
+ *@s: the pointer to the memory area
+ *@b: the byte to fill *s with
+ *@n: the amount of bytes to be filled
+ *Return: (s) a pointer to the memory area s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
@@ -9,10 +16,14 @@ char *_memset(char *s, char b, unsigned int n)
 	return (s);
 }
 
-void ffree(char **pp)
+/**
+ * f_free - frees a string of strings
+ * @pp: string of strings
+ */
+void f_free(char **pp)
 {
 	char **a = pp;
-	
+
 	if (!pp)
 		return;
 
@@ -22,6 +33,14 @@ void ffree(char **pp)
 	free(a);
 }
 
+/**
+ * _realloc - reallocates a block of memory
+ * @ptr: pointer to previous malloc'ated block
+ * @old_size: byte size of previous block
+ * @new_size: byte size of new block
+ *
+ * Return: pointer to da ol'block nameen.
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *p;
