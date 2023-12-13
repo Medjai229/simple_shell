ddd7e482cfe4119731b99f003c18e3f3675ff4ca
Malik <malikhussein229@gmail.com>
2023-12-13 09:07:59 -0800

    Fixing betty errors

--- str_func2.c
+++ str_func2.c
@@ -1,5 +1,12 @@
 #include "shell.h"
 
+/**
+ * _strcpy - copies a string
+ * @dest: the destination
+ * @src: the source
+ *
+ * Return: pointer to destination
+ */
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
@@ -8,7 +15,7 @@ char *_strcpy(char *dest, char *src)
 		return (dest);
 	while (src[i])
 	{
-		dest[i] = src [i];
+		dest[i] = src[i];
 		i++;
 	}
 
@@ -16,6 +23,12 @@ char *_strcpy(char *dest, char *src)
 	return (dest);
 }
 
+/**
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
+ *
+ * Return: pointer to the duplicated string
+ */
 char *_strdup(const char *str)
 {
 	int length = 0;
@@ -37,6 +50,12 @@ char *_strdup(const char *str)
 	return (ret);
 }
 
+/**
+ *_puts - prints an input string
+ *@str: the string to be printed
+ *
+ * Return: Nothing
+ */
 void _puts(char *str)
 {
 	int i = 0;
@@ -51,6 +70,13 @@ void _puts(char *str)
 	}
 }
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c)
 {
 	static int i;
