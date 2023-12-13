9459955a29569b08c27fea695d7c5e8a61a87750
MMarzoo <mmarzouk12378@gmail.com>
2023-12-13 08:36:29 -0800

    adding betty

--- hsh.c
+++ hsh.c
@@ -30,7 +30,7 @@ int hsh(info_t *info, char **av)
 			_putchar('\n');
 		free_info(info, 0);
 	}
-	write_history(info);
+	write_hist(info);
 	free_info(info, 1);
 	if (!interactive(info) && info->status)
 		exit(info->status);
@@ -137,7 +137,7 @@ void fork_cmd(info_t *info)
 	}
 	if (child_pid == 0)
 	{
-		if (execve(info->path, info->argv, get_environ(info)) == -1)
+		if (execve(info->path, info->argv, get_env(info)) == -1)
 		{
 			free_info(info, 1);
 			if (errno == EACCES)
