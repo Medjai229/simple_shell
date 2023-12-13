42a298f3112e5a0a3b50fdf8d7a5625746a91209
MMarzoo <mmarzouk12378@gmail.com>
2023-12-13 08:36:56 -0800

    adding betty

--- info.c
+++ info.c
@@ -1,6 +1,9 @@
 #include "shell.h"
 
-/***/
+/**
+ * clear_info - initializes info_t struct
+ * @info: struct address
+ */
 void clear_info(info_t *info)
 {
 	info->arg = NULL;
@@ -9,7 +12,11 @@ void clear_info(info_t *info)
 	info->argc = 0;
 }
 
-/***/
+/**
+ * set_info - initializes info_t struct
+ * @info: struct address
+ * @av: argument vector
+ */
 void set_info(info_t *info, char **av)
 {
 	int i = 0;
@@ -32,12 +39,16 @@ void set_info(info_t *info, char **av)
 			;
 		info->argc = i;
 
-		replace_alias(info);
-		replace_vars(info);
+		rep_alias(info);
+		rep_vars(info);
 	}
 }
 
-/***/
+/**
+ * free_info - frees info_t struct fields
+ * @info: struct address
+ * @all: true if freeing all fields
+ */
 void free_info(info_t *info, int all)
 {
 	ffree(info->argv);
